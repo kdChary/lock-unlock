ecdf8def456e6b4f3171147f34f1fabd8b0346bb
Siva Dinesh .K <ksd.ccbp4@gmail.com>
2024-04-23 04:01:47 +0000

    code tested and published

--- src/components/Unlock/index.js
+++ src/components/Unlock/index.js
@@ -1,1 +1,43 @@
-// Write your code here
+// importing the necessary hooks.
+import {useState} from 'react'
+
+// importing styled components.
+import {UnlockBg, ContentCard, Image, Text, Button} from './styledComponents'
+
+const Unlock = () => {
+  const [locked, changeLock] = useState(true)
+
+  const toggleLock = () => {
+    changeLock(prevState => !prevState)
+  }
+
+  return (
+    <UnlockBg>
+      {locked ? (
+        <ContentCard>
+          <Image
+            src="https://assets.ccbp.in/frontend/hooks/lock-img.png"
+            alt="lock"
+          />
+          <Text>Your Device is Locked</Text>
+          <Button type="button" onClick={toggleLock}>
+            Unlock
+          </Button>
+        </ContentCard>
+      ) : (
+        <ContentCard>
+          <Image
+            src="https://assets.ccbp.in/frontend/hooks/unlock-img.png"
+            alt="unlock"
+          />
+          <Text>Your Device is Unlocked</Text>
+          <Button type="button" onClick={changeLock}>
+            Lock
+          </Button>
+        </ContentCard>
+      )}
+    </UnlockBg>
+  )
+}
+
+export default Unlock
--- src/components/Unlock/styledComponents.js
+++ src/components/Unlock/styledComponents.js
@@ -1,1 +1,57 @@
 // Style your elements here
+import styled from 'styled-components'
+
+// styling main container
+export const UnlockBg = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(to bottom, #0b0c1e, #161617, #3c2940);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+// Styling Content holding container
+export const ContentCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: transparent;
+  border: none;
+
+  gap: 33px;
+  padding: 3vw;
+`
+
+// Styling text
+export const Text = styled.p`
+  color: #ffffff;
+  font-family: 'Roboto';
+  font-size: 15px;
+  font-weight: 600;
+  text-align: center;
+`
+
+// Styling button..
+export const Button = styled.button`
+  border-radius: 5px;
+  border: solid 2px #06b6d4;
+  background: #06b6d4;
+  color: #ffffff;
+
+  padding: 5px 9px;
+  font-family: 'Roboto';
+  font-size: 13px;
+  font-weight: 600;
+
+  outline: none;
+  cursor: pointer;
+`
+// Styling Image..
+export const Image = styled.img`
+  width: 123px;
+
+  @media (min-width: 768px) {
+    width: 223px;
+  }
+`
